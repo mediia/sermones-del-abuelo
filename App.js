f50f5bfa670b484861f1c360c2c6c20db5c805ad
Daniel Scholtus <daniel@scholtus.com.ar>
2018-04-09 09:25:31 -0300

    Managed to load HTML

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import db from './Database';
+import resolveAssetSource from 'resolveAssetSource';
+import HTML from 'react-native-render-html';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    fetch(resolveAssetSource(require('./sermones/la_justificacion_por_la_fe_laodicea_y_sus_males.html')).uri)
+      .then(response => response.text())
+      .then(response => this.setState({html: response}))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        {/*
+        { db.map((sermon, key) => 
+          <Text key={key}>
+            { sermon.title }
+          </Text>
+        )}
+        */}
+        { this.state.html ? 
+          <HTML html={this.state.html} />
+        : undefined }
       </View>
     );
   }
---
+++ Database.js
@@ -0,0 +1,9 @@
+import la_justificacion_por_la_fe_laodicea_y_sus_males from './sermones/la_justificacion_por_la_fe_laodicea_y_sus_males.html'
+
+export default [
+    {
+        serie: `La justificación por la fe.`,
+        title: `Laodicea y sus males.`,
+        content: la_justificacion_por_la_fe_laodicea_y_sus_males
+    }
+]
